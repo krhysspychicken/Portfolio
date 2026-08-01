3d8403e0aade0fd0318d3033370671ad6fd19bb6
Christian Dave Juliales <dave.juliales@gmail.com>
2026-08-01 19:51:05 +0800

    Panglahatan na bigkasan

---
+++ footer.js
@@ -0,0 +1,11 @@
+(function () {
+  const footer = document.createElement('footer');
+  footer.className = 'site-footer';
+  footer.innerHTML = `
+    <div class="site-footer__inner">
+      <p>© 2026 Krhyss Kringle Tuba-on · Multimedia & Creative Design</p>
+    </div>
+  `;
+
+  document.body.appendChild(footer);
+})();
